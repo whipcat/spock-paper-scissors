5ccca09998e78fb8d68930c1061c17ebc514acbc
Johnny Boersma <johnnyboersma187@gmail.com>
2020-03-25 09:56:12 +0100

    readme remove

---
+++ build/precache-manifest.4cc556db8e437912ccba4d65f352cd11.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "04d843fd9ea8cb6485325a75a240a3e3",
+    "url": "/spock-paper-scissors/index.html"
+  },
+  {
+    "revision": "811d2209d7839acb9711",
+    "url": "/spock-paper-scissors/static/css/main.d3e9406b.chunk.css"
+  },
+  {
+    "revision": "8de94b2f8052f40fdfce",
+    "url": "/spock-paper-scissors/static/js/2.e7d1af57.chunk.js"
+  },
+  {
+    "revision": "811d2209d7839acb9711",
+    "url": "/spock-paper-scissors/static/js/main.eb500b79.chunk.js"
+  },
+  {
+    "revision": "4b8b43d28af5904453d5",
+    "url": "/spock-paper-scissors/static/js/runtime~main.faf9e5c6.js"
+  }
+]);
